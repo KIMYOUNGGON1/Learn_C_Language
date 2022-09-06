56c3e5aa59547e6d686d0f841ecc1f4262b43b28
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-07 07:48:10 +0900

    2022-09-07 C 프로그래밍 - 입문부터 게임 개발까지 (2-4 실수형 변수)

--- main.c
+++ main.c
@@ -7,9 +7,10 @@ int main() {
     age = 13;
     printf("%d\n", age);*/
 
-    printf("1\n");
-//    printf("2\n"); //2는 범위에서 벗어나므로 주석 처리
-    printf("3\n");
-
+    //실수형 변수에 대한 예제
+    float f = 46.5f;
+    printf("%.2f\n", f);
+    double d = 4.428;
+    printf("%.2lf\n", d);
     return 0;
 }
