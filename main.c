b5032c1acf6abce7ff5811080e64df19e2c58938
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-07 07:55:05 +0900

    2022-09-07 C 프로그래밍 - 입문부터 게임 개발까지 (2-6 printf)

--- main.c
+++ main.c
@@ -12,9 +12,16 @@ int main() {
     printf("%.2f\n", f);
     double d = 4.428;
     printf("%.2lf\n", d);
-    */
+
     const int YEAR = 2000; //상수
     printf("태어난 년도 : %d\n", YEAR);
-    //YEAR = 2001;
+    //YEAR = 2001;*/
+
+    //printf
+    //연산
+    int add = 3 + 7; //10
+    printf("3 + 7 = %d\n", add);
+    printf("%d * %d = %d\n", 30, 79, 30 * 79);
+
     return 0;
 }
