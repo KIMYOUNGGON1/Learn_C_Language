dc580f6aacf44d96bc23c840587f301960d53cb9
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-08 08:23:58 +0900

    2022-09-08 C 프로그래밍 - 입문부터 게임 개발까지 (3-3 for)

--- main.c
+++ main.c
@@ -31,6 +31,7 @@ int main() {
     printf("b 는 %d\n", b);
     */
 
+    /*
     int i = 1;
     printf("Hello World %d\n", i++);
     printf("Hello World %d\n", i++);
@@ -41,7 +42,15 @@ int main() {
     printf("Hello World %d\n", i++);
     printf("Hello World %d\n", i++);
     printf("Hello World %d\n", i++);
-    printf("Hello World %d\n", i++);
+    printf("Hello World %d\n", i++); */
+
+    //반복문
+    //for, while, do while
+
+    //for (선언: 조건: 증감)
+    for (int i = 1; i <= 10; ++i) {
+        printf("Hello World %d\n", i);
+    }
 
     return 0;
 }
