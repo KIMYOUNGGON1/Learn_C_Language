c4af1650748bb7466be5d64852d8a15a6fcce86d
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-08 14:57:52 +0900

    2022-09-08 C 프로그래밍 - 입문부터 게임 개발까지 (3-8 이중 반복문 거꾸로 별)

--- main.c
+++ main.c
@@ -99,8 +99,18 @@ int main() {
     }
     */
 
-    for (int i = 0; i < 5; ++i) {
+    /* for (int i = 0; i < 5; ++i) {
         for (int j = 0; j <= i; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = i; j < 5 - 1 ; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++) {
             printf("*");
         }
         printf("\n");
