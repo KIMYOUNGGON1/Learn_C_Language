34477cc8758f88b92bdf31b9738a68ee5889b82a
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-08 14:47:33 +0900

    2022-09-08 C 프로그래밍 - 입문부터 게임 개발까지 (3-7 이중 반복문 파헤치기)

--- main.c
+++ main.c
@@ -90,11 +90,20 @@ int main() {
 //    2 x 5 = 10...
 //    ...
 //    9 X 9 = 81
+/*
     for (int i = 2; i <= 9 ; ++i) {
         printf("%d단 계산\n", i);
         for (int j = 1; j <= 9; ++j) {
             printf(" %d x %d = %d\n", i, j, i*j);
         }
+    }
+    */
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            printf("*");
+        }
+        printf("\n");
     }
 
 
