483e6695163402d7abb3fb78d5f0e2251c9e6388
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-13 07:53:43 +0900

    2022-09-13 C 프로그래밍 - 입문부터 게임 개발까지 (3-9 프로젝트 피라미드 쌓기)

--- main.c
+++ main.c
@@ -106,6 +106,7 @@ int main() {
         printf("\n");
     }*/
 
+    /*
     for (int i = 0; i < 5; i++) {
         for (int j = i; j < 5 - 1 ; j++) {
             printf(" ");
@@ -114,7 +115,24 @@ int main() {
             printf("*");
         }
         printf("\n");
+    }*/
+
+    //피라미드를 쌓아라 - 프로젝트
+
+    int floor;
+    printf("몇 층으로 쌓겠느냐?");
+    scanf("%d", &floor);
+
+    for (int i = 0; i < floor; i++) {
+        for (int j = i; j < floor - 1; j++) {
+            printf(" ");
+        }
+        for (int k = 0; k < i * 2 + 1; k++) {
+            printf("*");
+        }
+        printf("\n");
     }
+
 
 
     return 0;
