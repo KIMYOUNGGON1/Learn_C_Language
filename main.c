f735f626c7e8d8b683c6ca9bc4bb0226023a3edd
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-13 13:37:25 +0900

    2022-09-13 C 프로그래밍 - 입문부터 게임 개발까지 (5-8 반환값과 전달값이 있는 함수)

--- main.c
+++ main.c
@@ -7,7 +7,8 @@
 //void function_without_return();
 //int function_with_return();
 //void function_without_parameter();
-void function_with_params(int num1, int num2, int num3);
+//void function_with_params(int num1, int num2, int num3);
+int apple(int total, int ate); //전체 total 개에서 ate 개를 먹고 남은 수를 반환
 
 int main() {
 
@@ -332,7 +333,12 @@ int main() {
 //    function_without_parameter();
 
     //파라미터(전달값)가 있는 함수
-    function_with_params(35, 27, 13);
+//    function_with_params(35, 27, 13);
+
+    //파라미터(전달값)도 받고, 반환값이 있는 함수
+//    int ret = apple(5, 2); //5개의 사과 중에서 2개를 먹었어요.
+//    printf("사과 5개 중에 2개를 먹으면? %d 개가 남아요.\n", ret);
+    printf("사과 %d 개 중에 %d 개를 먹으면? %d 개가 남아요\n", 10, 4, apple(10, 4));
 
     return 0;
 }
@@ -362,7 +368,11 @@ int main() {
 //    printf("전달값이 없는 함수입니다.\n");
 //}
 
-void function_with_params(int num1, int num2, int num3) {
-    printf("전달값이 있는 함수이며, 전달받은 값은 %d, %d, %d 입니다.\n", num1, num2, num3);
-}
+//void function_with_params(int num1, int num2, int num3) {
+//    printf("전달값이 있는 함수이며, 전달받은 값은 %d, %d, %d 입니다.\n", num1, num2, num3);
+//}
 
+int apple(int total, int ate) {
+    printf("전달값과 반환값이 있는 함수입니다.\n");
+    return total - ate;
+}
