cfcdbb44e33b98c80a8baaff3318ac57df1c7366
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-14 07:47:04 +0900

    2022-09-14 C 프로그래밍 - 입문부터 게임 개발까지 (5-9 함수를 이용한 계산기)

--- main.c
+++ main.c
@@ -3,12 +3,16 @@
 #include <stdlib.h>
 
 //선언
-//void p(int num);
+void p(int num);
 //void function_without_return();
 //int function_with_return();
 //void function_without_parameter();
 //void function_with_params(int num1, int num2, int num3);
-int apple(int total, int ate); //전체 total 개에서 ate 개를 먹고 남은 수를 반환
+//int apple(int total, int ate); //전체 total 개에서 ate 개를 먹고 남은 수를 반환
+int add(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int divide(int num1, int num2);
 
 int main() {
 
@@ -338,15 +342,29 @@ int main() {
     //파라미터(전달값)도 받고, 반환값이 있는 함수
 //    int ret = apple(5, 2); //5개의 사과 중에서 2개를 먹었어요.
 //    printf("사과 5개 중에 2개를 먹으면? %d 개가 남아요.\n", ret);
-    printf("사과 %d 개 중에 %d 개를 먹으면? %d 개가 남아요\n", 10, 4, apple(10, 4));
+//    printf("사과 %d 개 중에 %d 개를 먹으면? %d 개가 남아요\n", 10, 4, apple(10, 4));
+
+    //계산기 함수
+    int num = 2;
+    num = add(num, 3);
+    p(num);
+
+    num = sub(num, 1);
+    p(num);
+
+    num = mul(num, 3);
+    p(num);
+
+    num = divide(num, 6);
+    p(num);
 
     return 0;
 }
 
 ////정의
-//void p(int num) {
-//    printf("num 은 %d 입니다\n", num);
-//}
+void p(int num) {
+    printf("num 은 %d 입니다\n", num);
+}
 
 // 전달값 : 36
 // 함수 : ㅁ + 4
@@ -372,7 +390,23 @@ int main() {
 //    printf("전달값이 있는 함수이며, 전달받은 값은 %d, %d, %d 입니다.\n", num1, num2, num3);
 //}
 
-int apple(int total, int ate) {
-    printf("전달값과 반환값이 있는 함수입니다.\n");
-    return total - ate;
+//int apple(int total, int ate) {
+//    printf("전달값과 반환값이 있는 함수입니다.\n");
+//    return total - ate;
+//}
+
+int add(int num1, int num2) {
+    return num1 + num2;
+}
+
+int sub(int num1, int num2) {
+    return num1 - num2;
+}
+
+int mul(int num1, int num2) {
+    return num1 * num2;
+}
+
+int divide(int num1, int num2) {
+    return num1 / num2;
 }
