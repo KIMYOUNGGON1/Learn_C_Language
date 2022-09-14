1a3fb7dbc3b9c8d9d9bb24ebbfae310a2a9e881a
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-15 08:26:33 +0900

    2022-09-15 C 프로그래밍 - 입문부터 게임 개발까지 [7-3 미션맨(포인터)의 등장

--- main.c
+++ main.c
@@ -21,5 +21,32 @@ int main() {
     printf("영희네 주소 : %d, 암호 : %d\n", &yh, yh);
     printf("민수네 주소 : %d, 암호 : %d\n", &ms, ms);
 
+    // 미션맨!
+
+    // 첫 번째 미션 : 아파트의 각 집에 방문하여 문에 적힌 암호 확인
+    int * missionMan; //포인터 변수
+    missionMan = &cs;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
+    missionMan = &yh;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
+    missionMan = &ms;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
+
+    // 두 번째 미션 : 각 암호에 3을 곱해라
+    missionMan = &cs;
+    *missionMan = *missionMan * 3;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
+    missionMan = &yh;
+    *missionMan = *missionMan * 3;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
+    missionMan = &ms;
+    *missionMan = *missionMan * 3;
+    printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", missionMan, *missionMan);
+
     return 0;
 }
