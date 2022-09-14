6d61ce80f4e51173f81962d91a7ac7a07960659f
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-15 08:19:57 +0900

    2022-09-15 C 프로그래밍 - 입문부터 게임 개발까지 (7-2 친구들의 주소)

--- main.c
+++ main.c
@@ -6,73 +6,20 @@
 
 int main() {
 
-    srand(time(NULL));
-    printf("\n\n === 아빠는 대머리 게임 === \n\n");
-    int answer; //사용자 입력값
-    int treatment = rand() % 4; //발모제 선택 (0-3)
+    // 포인터
 
-    int cntShowBottle = 0; //이번 게임에 보여줄 병 갯수
-    int prevCntShowBottle = 0; //앞 게임에 보여준 병 갯수
-    //서로 보여주는 병 갯수를 다르게 하여 정답률 향상 (처음에 2개 -> 다음에는 3개...)
+    // [철수] : 101호 -> 메모리 공간의 주소
+    // [영희] : 102호
+    // [민수] : 103호
+    // 각 문 앞에 '암호' 가 걸려 있음
 
-    //3번의 기회 (3번의 발모제 투여 시도)
-    for (int i = 1; i <= 3; i++) {
-        int bottle[4] = {0, 0, 0, 0}; //4개의 병
-        do {
-            cntShowBottle = rand() % 2 + 2; //보여줄 병 갯수 (0-1, +2 -> 2 or 3)
-        } while (cntShowBottle == prevCntShowBottle);
+    int cs = 1; // 암호
+    int yh = 2;
+    int ms = 3;
 
-        prevCntShowBottle = cntShowBottle;
-
-
-        int isIncluded = 0; //보여줄 병 중에 발모제가 포함되었는지 여부 (1이면 포함, 0이면 불포함)
-        printf(" > %d 번째 시도 : ", i);
-
-        //보여줄 병 종류를 선택
-        for (int j = 0; j < cntShowBottle; j++) {
-            int randBottle = rand() % 4; // 0-3
-
-            // 아직 선택되지 않은 병이면, 선택 처리
-            if (bottle[randBottle] == 0) {
-                bottle[randBottle] = 1;
-                if (randBottle == treatment) {
-                    isIncluded = 1;
-                }
-            }
-                //이미 선택된 병이면 중복이므로 다시 선택
-            else {
-                j--;
-            }
-        }
-
-        //사용자에게 문제 표시
-        for (int k = 0; k < 4; k++) {
-            if (bottle[k] == 1) {
-                printf("%d", k + 1);
-            }
-            printf("물약을 머리에 바릅니다.\n\n");
-
-            if (isIncluded == 1) {
-                printf(" >> 성공!! 머리가 났어요!!! \n");
-            } else {
-                printf(" >> 실패!! 머리가 나지 않았어요.... \n");
-            }
-
-            printf("\n ...... 계속 하려면 아무키나 누르세요 ....");
-            getchar();
-        }
-
-        printf("\n\n 발모제는 몇 번일까요?? ");
-        scanf("%d", &answer);
-
-        if (answer == treatment + 1) {
-            printf("\n 정답입니다!\n");
-        } else {
-            printf("\n >> 땡! 틀렸어요. 정답은 %d 입니다.\n", treatment + 1);
-        }
-    }
-
-
+    printf("철수네 주소 : %d, 암호 : %d\n", &cs, cs);
+    printf("영희네 주소 : %d, 암호 : %d\n", &yh, yh);
+    printf("민수네 주소 : %d, 암호 : %d\n", &ms, ms);
 
     return 0;
 }
