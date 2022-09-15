d654b022e420d7b65eafdad175e904f3c9ad44a6
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-15 11:12:17 +0900

    2022-09-15 C 프로그래밍 - 입문부터 게임 개발까지 [7-6 Swap]

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-
+void swap(int a, int b);
+void swap_addr(int * a, int * b);
 
 int main() {
 
@@ -89,6 +90,7 @@ int main() {
     printf("스파이 주소 : %d\n", &spy);
      */
 
+    /*
     // 배열 ?
     int arr[3] = {5, 10, 15};
     int * ptr = arr;
@@ -122,7 +124,47 @@ int main() {
     // *& 는 상쇄된다.
     printf("arr[0] 의 실제 값 : %d\n", *&*&*&*&*&arr[0]);
     printf("arr[0] 의 실제 값 : %d\n", arr[0]);
+     */
 
+    //SWAP
+    int a = 10;
+    int b = 20;
+    printf("a 의 주소 : %d\n", &a);
+    printf("b 의 주소 : %d\n", &b);
+
+    // a와 b의 값을 바꾼다.
+    printf("Swap 함수 전 => %d, b : %d\n", a, b);
+    swap(a, b);\
+    printf("Swap 함수 후 => a : %d, b : %d\n", a, b);
+
+    //값에 의한 복사 (Call by Value) -> 값만 복사한다는 의미
+
+    //주소값을 넘기면? 메모리 공간에 있는 주소값 자체를 넘기면.... 철수네처럼
+    printf("(주소값 전달)Swap 함수 전 => %d, b : %d\n", a, b);
+    swap_addr(&a, &b);
+    printf("Swap 함수 후 => a : %d, b : %d\n", a, b);
 
     return 0;
+}
+
+void swap(int a, int b) {
+
+    printf("Swap 함수 내 a 의 주소 : %d\n", &a);
+    printf("Swap 함수 내 b 의 주소 : %d\n", &b);
+
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Swap 함수 후 => a : %d, b : %d\n", a, b);
+}
+
+void swap_addr(int * a, int * b) {
+
+    printf("Swap_addr 함수 내 a 의 주소 : %d\n", &a);
+    printf("Swap_addr 함수 내 b 의 주소 : %d\n", &b);
+
+    int temp = * a;
+    * a = * b;
+    * b = temp;
+    printf("(주소값 전달)Swap_addr 함수 후 => a : %d, b : %d\n", * a, * b);
 }
