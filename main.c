0066836dd860cc811a17731855a28a7a0f43a416
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-20 08:16:32 +0900

    2022-09-20 C 프로그래밍 - 입문부터 게임 개발까지 [8-6 프로젝트(전반전)]

--- main.c
+++ main.c
@@ -2,109 +2,100 @@
 #include <time.h>
 #include <stdlib.h>
 
+// 10마리의 서로 다른 동물 (각 카드 2장씩)
+// 사용자로부터 2개의 입력값을 받아서 -> 같은 동물 찾으면 카드 뒤집기
+// 모든 동물 쌍을 찾으면 게임 종료
+// 총 실패 횟수 알려주기
+
+int arrayAnimal[4][5]; //카드 지도 (20장의 카드)
+char * strAnimal[10];
+
+void initAnimalArray();
+void initAnimalName();
+void shuffleAnimal();
+int getEmptyPosition();
+int conv_pos_x(int x);
+int conv_pos_y(int y);
+
 int main() {
 
-    // 다차원 배열 Multidimensional Array
-//    int i;
-    // ㅁ
+    srand(time(NULL));
 
-//    int arr[5];
-    //ㅁㅁㅁㅁㅁ
-    //[0][1][2][3][4]
+    initAnimalArray();
+    initAnimalName();
 
-//    int arr2[2][5];
-    //ㅁㅁㅁㅁㅁ
-    //ㅁㅁㅁㅁㅁ
-    //[0,0][0,1][0,2][0,3][0,4] -> ex) arr2[0][0]
-    //[1,0][1,1][1,2][1,3][1,4] -> ex) arr2[1][4]
-
-//    int arr3[4][2];
-    //ㅁㅁ
-    //ㅁㅁ
-    //ㅁㅁ
-    //ㅁㅁ
-
-    //[0,0][0,1] -> ex) arr3[0][1]
-    //[1,0][1,1]
-    //[2,0][2,1]
-    //[3,0][3,1] -> ex) arr3[3][0]
-
-//    int arr4[3][3][3];
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-    //ㅁㅁㅁ
-
-    // [0,0,0][0,0,1][0,0,2]
-    // [0,1,0][0,1,1][0,1,2]
-    // [0,2,0][0,2,1][0,2,2]
-
-    // [1,0,0][1,0,1][1,0,2]
-    // [1,1,0][1,1,1][1,1,2]
-    // [1,2,0][1,2,1][1,2,2]
-
-    // [2,0,0][2,0,1][2,0,2]
-    // [2,1,0][2,1,1][2,1,2]
-    // [2,2,0][2,2,1][2,2,2]
-
-//    int arr[5] = {1, 2, 3, 4, 5};
-//    int arr2[2][5] = {
-//            {1, 2, 3, 4, 5},
-//            {1, 2, 3, 4, 5}
-//    };
-
-/*
-    int arr3[4][2] = {
-            {1,2},
-            {3,4},
-            {5,6},
-            {7,8}
-    };
-
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("2차원 배열 (%d, %d) 의 값 : %d\n", i, j, arr3[i][j]);
-        }
-        printf("\n");
-    }
-    */
-
-    int arr4[3][3][3] = {
-            {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
-            },
-            {
-                {11,12,13},
-                {14,15,16},
-                {17,18,19}
-            },
-            {
-                {21,22,23},
-                {24,25,26},
-                {27,28,29}
-            }
-    };
-
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            for (int k =0; k < 3; k++) {
-                printf("3차원 배열 (%d, %d, %d) 의 값 : %d\n\n", i, j, k, arr4[i][j][k] );
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
+    shuffleAnimal();
 
     return 0;
 }
 
+void initAnimalArray() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            arrayAnimal[i][j] = -1;
+        }
+
+    }
+}
+
+void initAnimalName() {
+    strAnimal[0] = "원숭이";
+    strAnimal[1] = "하마";
+    strAnimal[2] = "강아지";
+    strAnimal[3] = "고양이";
+    strAnimal[4] = "돼지";
+
+    strAnimal[5] = "코끼리";
+    strAnimal[6] = "기린";
+    strAnimal[7] = "낙타";
+    strAnimal[8] = "타조";
+    strAnimal[9] = "호랑이";
+
+}
+
+void shuffleAnimal() {
+    // ㅁㅁㅁㅁㅁ
+    // ㅁㅁㅁㅁㅁ
+    // ㅁㅁㅁㅁㅁ
+    // ㅁㅁㅁㅁㅁ
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 2; j++) {
+            int pos = getEmptyPosition();
+            int x = conv_pos_x(pos);
+            int y = conv_pos_y(pos);
+
+            arrayAnimal[x][y] = i;
+        }
+    }
+}
+
+// 좌표에서 빈 공간 찾기
+int getEmptyPosition() {
+    // ㅁㅁㅁㅁㅁ 0 1 2 3 4
+    // ㅁㅁㅁㅁㅁ 5 6 7 8 9
+    // ㅁㅁㅁㅁㅁ 10 11 12 13 14
+    // ㅁㅁㅁㅁㅁ 15 16 17 18 19
+    while (1) {
+        int randPos = rand() % 20; // 0 - 19 사이의 숫자 반환
+        //19 -> (3,4)
+        int x = conv_pos_x(randPos);
+        int y = conv_pos_y(randPos);
+
+        if (arrayAnimal[x][y] == -1) {
+            return randPos;
+        }
+    }
+    return 0;
+}
+
+int conv_pos_x(int x) {
+
+    // 19 -> (3, 4)
+    return x / 5;
+
+}
+
+int conv_pos_y(int y) {
+    // 19 -> 19 / 5 ? 몫은 3, 나머지 4
+    return y % 5; // y를 5로 나눈 나머지 값
+}
