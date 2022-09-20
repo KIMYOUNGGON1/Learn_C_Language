06193cccd8fcdc9c82e75ceb9b20c902e8e3b9e8
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-21 08:35:24 +0900

    2022-09-21 C 프로그래밍 - 입문부터 게임 개발까지 [9-3 구조체 사용]

--- main.c
+++ main.c
@@ -33,6 +33,27 @@ int main() {
     int price2 = 50;
     char * company2 = "너도회사";
 
+    // 구조체 사용
+    struct GameInfo gameInfo1;
+    gameInfo1.name = "나도게임";
+    gameInfo1.year = 2017;
+    gameInfo1.price = 50;
+    gameInfo1.company = "나도회사";
+
+    // 구조체 출력
+    printf("-- 게임 출시 정보 --\n");
+    printf(" 게임명 : %s\n", gameInfo1.name);
+    printf(" 발매년도 : %.d\n", gameInfo1.year);
+    printf(" 가격 : %d\n", gameInfo1.price);
+    printf(" 제작사 : %s\n", gameInfo1.company);
+
+    // 구조체를 배열처럼 초기화
+    struct GameInfo gameInfo2 = {"너도게임", 2017, 100, "너도회사"};
+    printf("-- 또다른 게임 출시 정보 --\n");
+    printf(" 게임명 : %s\n", gameInfo2.name);
+    printf(" 발매년도 : %.d\n", gameInfo2.year);
+    printf(" 가격 : %d\n", gameInfo2.price);
+    printf(" 제작사 : %s\n", gameInfo2.company);
 
     return 0;
 }
