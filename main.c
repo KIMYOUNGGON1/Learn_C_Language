564f6aa33affc406007aa43040cbd7911f9fc59d
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-22 07:56:40 +0900

    2022-09-22 C 프로그래밍 - 입문부터 게임 개발까지 [9-6 구조체 안의 구조체]

--- main.c
+++ main.c
@@ -7,6 +7,9 @@ struct GameInfo {
     int year;
     int price;
     char *company;
+
+    struct GameInfo * friendGame; //연관 업체 게임
+
 };
 
 int main() {
@@ -73,6 +76,14 @@ int main() {
     printf(" 발매년도 : %.d\n", gamePtr -> year);
     printf(" 가격 : %d\n", gamePtr -> price);
     printf(" 제작사 : %s\n", gamePtr -> company);
+
+    // 연관 업체 게임 소개
+    gameInfo1.friendGame = &gameInfo2;
+    printf("\n\n-- 연관 업체의 게임 출시 정보 --\n");
+    printf(" 게임명 : %s\n",  gameInfo1.friendGame -> name);
+    printf(" 발매년도 : %.d\n",  gameInfo1.friendGame -> year);
+    printf(" 가격 : %d\n",  gameInfo1.friendGame -> price);
+    printf(" 제작사 : %s\n",  gameInfo1.friendGame -> company);
 
 
     return 0;
