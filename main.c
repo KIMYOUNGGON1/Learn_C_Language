f7ca03644b577ab344fa0cc7b0155aebaa39db04
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-23 09:20:32 +0900

    2022-09-23 C 프로그래밍 - 입문부터 게임 개발까지 [10-3 파일 읽기 fgets]

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main() {
 
     // fputs, fgets 쌍
     char line[MAX]; // char line[10000]
+    //파일에 쓰기
     FILE *file = fopen("C:\\Users\\dudrh\\test\\test1.txt", "wb"); // r (읽기) w (쓰기) a (이어쓰기) // t(text) b(binary data)
     if (file == NULL) {
         printf("파일 열기 실패\n");
@@ -22,6 +23,16 @@ int main() {
     fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
     fputs("잘 적히는지 확인해주세요\n", file);
 
+    //파일 읽기
+    FILE * file2 = fopen("C:\\Users\\dudrh\\test\\test1.txt", "rb"); // r (읽기) w (쓰기) a (이어쓰기) // t(text) b(binary data)
+    if (file == NULL) {
+        printf("파일 열기 실패\n");
+        return 1;
+    }
+
+    while (fgets(line, MAX, file2) != NULL) {
+        printf("%s", line);
+    }
     // 파일을 열고 나서 닫지 않은 상태에서 어떤 프로그램에 문제가 생기면?
     // 데이터 손실 발생 가능 ! 그래서 항상 파일은 닫아주는 습관을 들여주세요.
     fclose(file);
