f5683b17b39267a3384f71f865fd392d3535b989
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-26 08:41:00 +0900

    2022-09-26 C 프로그래밍 - 입문부터 게임 개발까지 [10-6 프로젝트(전반전)]

--- main.c
+++ main.c
@@ -4,76 +4,41 @@
 #include <time.h>
 #include <stdlib.h>
 
+// 비밀번호를 입력 받아서
+// 맞는 경우? 비밀 일기를 읽어와서 보여주고, 계속 작성하도록 합니다.
+// 틀린 경우? 경고 메시지를 표시하고 종료합니다.
+
 #define MAX 10000
+
 int main() {
 
-    // 파일 입출력
-    // 파일에 어떤 데이터를 저장
-    // 파일에 저장된 데이터를 불러오기
+    // fgets, fputs 활용
+    char line[MAX]; // 파일에서 불러온 내용을 저장할 변수
+    char contents[MAX]; // 일기장에 입력할 내용
+    char password[20]; // 비밀번호 입력
+    char c; // 비밀번호 입력 할 때 키값 확인용(* / 마스킹)
 
-    // fputs, fgets 쌍 -> 문자열 저장
-    char line[MAX]; // char line[10000]
-    //파일에 쓰기
-//    FILE *file = fopen("C:\\Study\\cmake-build-debug\\test\\test1.txt", "wb"); // r (읽기) w (쓰기) a (이어쓰기) // t(text) b(binary data)
-//    if (file == NULL) {
-//        printf("파일 열기 실패\n");
-//        return 1;
-//    }
-//
-//    fputs("fputs 를 이용해서 글을 적어볼게요\n", file);
-//    fputs("잘 적히는지 확인해주세요\n", file);
+    printf("'비밀일기'에 오신 것을 환영합니다\n");
+    printf("비밀번호를 입력하세요 : ");
 
-    //파일 읽기
-//    FILE * file = fopen("C:\\Study\\cmake-build-debug\\test\\test1.txt", "rb"); // r (읽기) w (쓰기) a (이어쓰기) // t(text) b(binary data)
-//    if (file == NULL) {
-//        printf("파일 열기 실패\n");
-//        return 1;
-//    }
-//
-//    while (fgets(line, MAX, file) != NULL) {
-//        printf("%s", line);
-//    }
-//    // 파일을 열고 나서 닫지 않은 상태에서 어떤 프로그램에 문제가 생기면?
-//    // 데이터 손실 발생 가능 ! 그래서 항상 파일은 닫아주는 습관을 들여주세요.
-//    fclose(file);
+    // getchar() / getch() 의 차이?
+    // getchar() : 엔터를 입력ㅂ다아야 동작을 한다.
+    // getch() : 키 입력 시 바로바로 동작을 한다.
 
-    // fprintf, fscanf 쌍
-    // printf("%d %d %s ...")
-    // scanf("%d, %d", &num1, ...)
-    int num[6] = {0, 0, 0, 0, 0, 0};
-    int bonus = 0; //보너스 번호
-    char str1[MAX];
-    char str2[MAX];
+    int i = 0;
+    while (1) {
+        c = getchar();
+        if (c == 13) //Enter  -> 비밀번호 입력 종
+        {
+            password[i] = '\0';
+            break;
+        } else { // 비밀번호 입력 중
+            printf("*");
+            password[i] = c;
+        }
+        i++;
 
-    // 파일에 쓰기
-//    FILE * file = fopen("C:\\Study\\cmake-build-debug\\test\\test2.txt", "wb");
-//    if (file = NULL) {
-//        printf("파일 열기 실패\n");
-//        return 1;
-//    }
-//
-//    // 로또 추첨 번호 저장
-//    fprintf(file, "%s %d %d %d %d %d %d\n", "추첨번호 ", 1, 2, 3, 4, 5, 6);
-//    fprintf(file, "%s %d\n", "보너스번호 ", 7);
-
-    // 파일 읽기
-    FILE * file = fopen("C:\\Study\\cmake-build-debug\\test\\test2.txt", "rb");
-    if (file = NULL) {
-        printf("파일 열기 실패\n");
-        return 1;
     }
-
-    fscanf(file, "%s %d %d %d %d %d %d", str1,
-           &num[0], &num[1], &num[2], &num[3], &num[4], &num[5]);
-    printf("%s %d %d %d %d %d %d\n", str1,
-           num[0], num[1], num[2], num[3], num[4], num[5]);
-
-    fscanf(file, "%s %d", str2, &bonus);
-    printf("\"%s %d\n", str2, bonus);
-
-    fclose(file);
-
-
 
     return 0;
 }
