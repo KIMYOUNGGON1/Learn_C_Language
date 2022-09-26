f1afea3ca96cd494420dbbb1858f8e4dd540ef34
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-09-27 08:10:01 +0900

    2022-09-27 C 프로그래밍 - 입문부터 게임 개발까지 [10-7 프로젝트(후반전)]

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
 // 비밀번호를 입력 받아서
 // 맞는 경우? 비밀 일기를 읽어와서 보여주고, 계속 작성하도록 합니다.
@@ -27,7 +29,7 @@ int main() {
 
     int i = 0;
     while (1) {
-        c = getchar();
+        c = getch();
         if (c == 13) //Enter  -> 비밀번호 입력 종
         {
             password[i] = '\0';
@@ -38,6 +40,41 @@ int main() {
         }
         i++;
 
+    }
+
+    //비밀번호 : 나도코딩 skehzheld
+    printf("\n\n === 비밀번호 확인 중... === \n\n");
+    if (strcmp(password, "skehzheld") == 0) // 비밀번호 일치
+    {
+        printf(" === 비밀번호 확인 완료 === \n\n");
+        char *fileName = "C:\\Study\\cmake-build-debug\\test\\secretediary.txt";
+        FILE *file = fopen(fileName, "a+b");
+        //파일이 없으면 생성, 파일이 있으면 append 를 한다. (뒤에서부터 내용을 추가한다.)
+        if (file = NULL) {
+            printf("파일 열기 실패\n");
+            return 1;
+        }
+        while (fgets(line, MAX, file) != NULL) {
+            printf("%s", line);
+        }
+        printf("\n\n 내용을 계속 작성하세요 ! 종료하시려면 EXIT 를 입력하세요\n");
+
+        while (1) {
+            scanf("%[^\n]", contents); // 새 줄(\n)이 나오기 전까지 읽어드림 (한 문장씩)
+            getchar(); // Enter 입력 (\n) Flush 처리
+
+            if (strcmp(contents, "EXIT") == 0) {
+                printf("비밀일기 입력을 종료합니다\n\n");
+                break;
+            }
+            fputs(contents, file);
+            fputs("\n", file); //Enter 를 위에서 무시 처리 하였으므로 임의로 추가
+        }
+        fclose(file);
+    }
+    else { // 비밀번호 틀린 경우
+        printf(" === 비밀번호가 틀렸어요 ===\n\n");
+        printf(" 꺅!! 당신 누구야?!! 감히 내 일기장을 !!! \n\n\n");
     }
 
     return 0;
